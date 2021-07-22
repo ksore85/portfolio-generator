b9d23f79be2ca52bbdad11d49604be05bd1bb041
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-07-21 22:35:40 -0500

    when method

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ const promptUser = () => {
         }
       }
     },
-
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
     {
       type: 'input',
       name: 'about',
@@ -125,16 +130,11 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     console.log(portfolioData);
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
 
